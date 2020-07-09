bc56c689705845936356a19d95f5be609677f1a8
YaIsrak <61416887+YaIsrak@users.noreply.github.com>
2020-07-09 12:57:31 +0600

    Initial commit

--- JS/color.js
+++ JS/color.js
@@ -14,6 +14,16 @@ function changeHue(){
     document.getElementById('range-st').style.backgroundImage = 'linear-gradient('+"to left" + "," + color2 + "," + 'rgb(133, 133, 133)' +')';
     document.getElementById('range-value').style.backgroundImage = 'linear-gradient('+"to left" + "," + 'white' + ',' + color2 + "," + 'black' +')';
 
+    //color box
+    document.getElementById("select1").style.backgroundColor = 'hsl('+ hue1 + ',' + '100'+'%' + ',' + '50'+'%' +')';
+    document.getElementById("select2").style.backgroundColor = 'hsl('+ hue1 + ',' + '50'+'%' + ',' + '50'+'%' +')';
+    document.getElementById("select3").style.backgroundColor = 'hsl('+ hue1 + ',' + '0'+'%' + ',' + '50'+'%' +')';
+    document.getElementById("select4").style.color = 'hsl('+ hue1 + ',' + '100'+'%' + ',' + '50'+'%' +')';
+
+    document.getElementById("select5").style.backgroundColor = 'hsl('+ hue1 + ',' + '100'+'%' + ',' + '50'+'%' +')';
+    document.getElementById("select6").style.backgroundColor = 'hsl('+ hue1 + ',' + '100'+'%' + ',' + '80'+'%' +')';
+    document.getElementById("select7").style.backgroundColor = 'hsl('+ hue1 + ',' + '100'+'%' + ',' + '30'+'%' +')';
+
     //Text Show
     document.getElementById('text-color').innerHTML = color1;
     document.getElementById('text-color2').innerHTML = color1;
